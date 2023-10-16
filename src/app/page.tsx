8120617cea7f27e55dff81b0f186d98189e84a80
madomaga <marcin.domagala@capgemini.com>
2023-10-16 15:21:14 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
     image
   );
   const [predictions, setPredictions] = useState<Array<Predictions>>();
+  const [active, setActive] = useState<string>("image");
 
   const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -44,9 +45,26 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="sfixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono font-bold">Image Prediction</code>
-        </p>
+        <div className="flex gap-4 py-4">
+          <p
+            onClick={() => setActive("image")}
+            className={`border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 ${
+              active === "image" ? "bg-gradient-to-b from-zinc-500" : null
+            }`}
+          >
+            <code className="font-mono font-bold">Image Prediction</code>
+          </p>
+
+          <p
+            onClick={() => setActive("number")}
+            className={`border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 ${
+              active === "number" ? "bg-gradient-to-b from-zinc-500" : null
+            }`}
+          >
+            <code className="font-mono font-bold">Number Prediction</code>
+          </p>
+        </div>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
